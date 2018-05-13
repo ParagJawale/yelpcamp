ae246ba3fbd54ff6f7738eedc62640065765a1a5
Parag Jawale <paragjawale24@gmail.com>
2018-05-13 21:59:09 +0000

    modefied user.js

--- models/user.js
+++ models/user.js
@@ -3,12 +3,8 @@ var mongoose = require("mongoose"),
     
 var UserSchema = new mongoose.Schema({
     username:String,
-<<<<<<< HEAD
     password:String,
     isAdmin: {type:Boolean, default: false}
-=======
-    password:String
->>>>>>> 6c3c71fa205fc31f731c76d00a3c6c4293b7515a
     
 });
 
